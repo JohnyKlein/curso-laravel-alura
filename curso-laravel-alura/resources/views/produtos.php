05da9a4ea72458a88da54b07901691a002bbea68
Johny Klein <johnyklein@hotmail.com>
2018-07-24 20:09:44 -0300

    View de produtos

--- curso-laravel-alura/resources/views/produtos.php
+++ curso-laravel-alura/resources/views/produtos.php
@@ -1,19 +1,23 @@
 <html>
+<head>
+    <link href="/css/app.css" rel="stylesheet">
+    <title>Controle de estoque</title>
+</head>
 <body>
-    <h1>Listagem de produtos</h1>
-    <div class="col-xs-12">
-        <?php foreach ($produtos as $p): ?>
-            <div class="col-xs-12">
-                <h4 style="display:inline">Nome: </h4>
-                <?= $p->nome ?>
-                <h4 style="display:inline">Valor: </h4> 
-                <?= $p->valor ?>
-                <h4 style="display:inline">Descricao: </h4>
-                <?= $p->descricao ?>
-                <h4 style="display:inline">Quantidade: </h4>
-                <?= $p->quantidade ?>
-            </div>
-        <?php endforeach; ?>
+    <div class="container">
+        <div class="row">
+            <h1>Listagem de produtos</h1>
+            <table class="table table-hover table-striped table-bordered">
+                <?php foreach ($produtos as $p): ?>
+                    <tr>
+                        <td><?= $p->nome ?></td>
+                        <td><?= $p->valor ?></td> 
+                        <td><?= $p->descricao ?></td>
+                        <td><?= $p->quantidade ?></td>
+                    </tr>
+                <?php endforeach; ?>
+            </table>
+        </div>
     </div>
 </body>
 </html>
