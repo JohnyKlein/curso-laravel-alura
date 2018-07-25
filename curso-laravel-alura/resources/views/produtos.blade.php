84d0f3ed2d5b23165b861d9f46898b133a71bcd9
Johny Klein <johnyklein@hotmail.com>
2018-07-25 20:47:34 -0300

    Tratativa para lista vazia

--- curso-laravel-alura/resources/views/produtos.blade.php
+++ curso-laravel-alura/resources/views/produtos.blade.php
@@ -4,19 +4,23 @@
 <div class="container">
     <div class="row">
         <h1>Listagem de produtos</h1>
-        <table class="table table-hover table-striped table-bordered">
-            @foreach ($produtos as $p)
-                <tr class="{{$p->quantidade <= 1 ? 'danger' : ''}}">
-                    <td>{{ $p->nome }}</td>
-                    <td>{{ $p->valor }}</td> 
-                    <td>{{ $p->descricao }}</td>
-                    <td>{{ $p->quantidade }}</td>
-                    <td>
-                        <a href="/produtos/detalhaProduto/{{$p->id}}">Visualizar</a>
-                    </td>
-                </tr>
-            @endforeach
-        </table>
+        @if (empty($produtos))
+            <p>Nao ha nenhum produto cadastrado!</p>
+        @else
+            <table class="table table-hover table-striped table-bordered">
+                @foreach ($produtos as $p)
+                    <tr class="{{$p->quantidade <= 1 ? 'danger' : ''}}">
+                        <td>{{ $p->nome }}</td>
+                        <td>{{ $p->valor }}</td> 
+                        <td>{{ $p->descricao }}</td>
+                        <td>{{ $p->quantidade }}</td>
+                        <td>
+                            <a href="/produtos/detalhaProduto/{{$p->id}}">Visualizar</a>
+                        </td>
+                    </tr>
+                @endforeach
+            </table>
+        @endif
     </div>
 </div>
 @stop
