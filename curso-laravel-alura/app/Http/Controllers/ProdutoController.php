7db0dfddc138cdfcb706bde0f4fa81206ccadf37
Johny Klein <johnyklein@hotmail.com>
2018-07-25 20:58:33 -0300

    Organizando diretorio das views

--- curso-laravel-alura/app/Http/Controllers/ProdutoController.php
+++ curso-laravel-alura/app/Http/Controllers/ProdutoController.php
@@ -8,13 +8,13 @@ class ProdutoController extends Controller {
     public function listaProdutos(){
         $produtos = DB::select("SELECT * FROM produtos");
         
-        return view("produtos")->withProdutos($produtos);
+        return view("produto/produtos")->withProdutos($produtos);
     }
 
     public function detalhaProduto() {
         $id = Request::route("id");
         $produto = DB::select("SELECT * FROM produtos WHERE id = ?", [$id]);
 
-        return view("produtoDetalhe")->withProduto($produto[0]);
+        return view("produto/produtoDetalhe")->withProduto($produto[0]);
     }
 }
